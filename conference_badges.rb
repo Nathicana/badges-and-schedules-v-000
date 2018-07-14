0c28df9d27a32a7112605fd66b38c06617add08c
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-14 22:07:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ end
 def batch_badge_creator(attendees)
   badges = []
   attendees.each do |name| 
-    badges << return badge_maker(name)
+    badges << return "Hello, my name is #{name}."
   badges
  end 
 end
