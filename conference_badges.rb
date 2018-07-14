b4455f80ebaa41b2dab9b1c21498ab6f05cf24d2
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-14 23:44:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,11 +21,11 @@ def printer(attendees)
 
 batch_badge_creator(attendees).each do |badges|
   puts badges
+ end 
  
  assign_rooms(speakers).each do |room_assignments|
    puts room_assignments
    
-  
-end
+  end
 
 
